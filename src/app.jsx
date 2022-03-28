9bcf310ce53d2456c7e0894427869e2a1468443b
j2khub <skyskyma@gmail.com>
2022-03-28 22:51:40 +0900

    j2k_tube_first

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,26 @@
-import './app.css';
+import { useEffect, useState } from "react";
+import "./app.css";
+import Video_list from "./components/video_list/video_list";
 
 function App() {
-  return <h1>Hello React</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyDdYdDfKey3ljQI6fLApOEQZBKP3swjo5k",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <Video_list videos={videos} />;
 }
 
 export default App;
---
+++ src/components/video_item/video_item.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+
+const Video_item = (props) => <h1>{props.video.snippet.title}</h1>;
+export default Video_item;
